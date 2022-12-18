797bb9d9e4a62c63158d9eb82777bf096899a6a6
Racine Fellipe <racine.fell@gmail.com>
2022-12-18 15:15:45 -0300

    criando arquivo js e criando id para braços

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
+
+somar.addEventListener("click", (evento)=>{
+    braco.value = parseInt(braco.value) + 1;
+
+});
+
+subtrair.addEventListener("click", (evento)=>{
+    braco.value = parseInt(braco.value) - 1;
+
+});
